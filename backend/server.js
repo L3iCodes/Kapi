f60deee07db6f70cb706ec3e5b67f0d821b65079
L3i <118181023+L3iCodes@users.noreply.github.com>
2025-08-28 09:19:10 +0800

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+const ORIGIN_CLIENT = process.env.ORIGIN_CLIENT;
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+
+// Setup CORS
+app.use(cors({
+    origin: ORIGIN_CLIENT,
+    credentials: true
+}));
+
+// Setup Express Server
+app.listen(PORT, () => {
+    console.log('Seever is running at https://localhost:' + PORT)
+});
